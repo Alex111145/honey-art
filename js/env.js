d13f850f22560b5c3430cdbdd5dc1d65c6686a0a
Ale <159631777+Alex111145@users.noreply.github.com>
2025-11-23 22:43:15 +0100

    Delete js/env.js

--- js/env.js
+++
@@ -1,8 +0,0 @@
-// js/env.js
-window.env = {
-    // Il tuo URL Supabase
-    SUPABASE_URL: 'https://zngxjvjoffuptwqbjegn.supabase.co',
-    
-    // La tua Chiave API (anon public)
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuZ3hqdmpvZmZ1cHR3cWJqZWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MjkxODcsImV4cCI6MjA3OTUwNTE4N30.nux31Ee7-xFXBOKlNfHUECy4o4TaEZtVvZKuO-DbH6E'
-};
